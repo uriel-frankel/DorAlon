f83b5a369298977bcef64fa1263ddfb42f2e4a59
Uriel Frankel <uriel@houzz.com>
2020-05-04 17:15:12 +0300

    buttons in home screen

---
+++ app/src/main/java/frankel/uriel/doralon/FirstScreenLayout.kt
@@ -0,0 +1,28 @@
+package frankel.uriel.doralon
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class FirstScreenLayout(
+    context: Context,
+    attrs: AttributeSet?,
+    defStyleAttr: Int,
+    defStyleRes: Int
+) : FrameLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : this(context, attrs, 0, 0)
+
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val params = buttonsContainer.layoutParams as FrameLayout.LayoutParams
+        params.leftMargin = (bg.measuredWidth / 11f).toInt()
+        params.topMargin = (bg.measuredHeight / 7f).toInt()
+
+    }
+}
