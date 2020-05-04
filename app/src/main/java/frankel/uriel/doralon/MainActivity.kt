f42f2ebc9ae943dc655179a3e6ac9cd2e4ee1a13
Uriel Frankel <uriel@houzz.com>
2020-05-04 22:11:20 +0300

    facebook homepage

--- app/src/main/java/frankel/uriel/doralon/MainActivity.kt
+++ app/src/main/java/frankel/uriel/doralon/MainActivity.kt
@@ -1,9 +1,12 @@
 package frankel.uriel.doralon
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +24,20 @@ class MainActivity : AppCompatActivity() {
 
         agreement.setOnClickListener {
             startActivity(Intent(this@MainActivity, AgreementActivity::class.java))
+        }
+
+        homepage.setOnClickListener {
+            val url = "https://www.doralon.co.il/"
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(i)
+        }
+
+        facebook.setOnClickListener {
+            val url = "https://www.facebook.com/dor.alon.il/"
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(i)
         }
 
 
