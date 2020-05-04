6429e40df7d8d2ba7814c23322ba7fd4247bb8e3
Uriel Frankel <uriel@houzz.com>
2020-05-04 08:37:33 +0300

    added waze and 24/7

--- app/src/main/java/frankel/uriel/doralon/StationAdapter.kt
+++ app/src/main/java/frankel/uriel/doralon/StationAdapter.kt
@@ -1,12 +1,18 @@
 package frankel.uriel.doralon
 
 import Station
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
 
 class StationAdapter : RecyclerView.Adapter<StationAdapter.StationViewHolder>() {
 
@@ -26,16 +32,42 @@ class StationAdapter : RecyclerView.Adapter<StationAdapter.StationViewHolder>() 
     override fun getItemCount() = list?.size ?: 0
 
     override fun onBindViewHolder(holder: StationViewHolder, position: Int) {
-        holder.name?.text = list?.get(position)?.node_title
+        list?.get(position)?.let {
+            holder.station = it
+            holder.name?.text = it.node_title
+            if (it.node_data_field_alonit_field_24hrs_value == 1) {
+                holder.open247?.visibility = View.VISIBLE
+            } else {
+                holder.open247?.visibility = View.GONE
+            }
+        }
+
     }
 
     class StationViewHolder : ViewHolder {
 
-
+        var open247: View? = null
+        var navigateWithWaze: Button? = null
         var name: TextView? = null
+        var station: Station? = null
 
         constructor(view: View) : super(view) {
             name = view.findViewById(R.id.name)
+            open247 = view.findViewById(R.id.open247)
+            navigateWithWaze = view.findViewById(R.id.navigate)
+            navigateWithWaze?.setOnClickListener {
+                try {
+                    val url =
+                        "https://waze.com/ul?ll=" + station?.location_latitude + "," + station?.location_longitude + "&navigate=yes"
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                    view.context.startActivity(intent)
+                } catch (ex: ActivityNotFoundException) {
+                    val intent =
+                        Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.waze"))
+                    view.context.startActivity(intent)
+                }
+            }
+
         }
     }
 }
